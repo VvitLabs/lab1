1095eb8688e8ea4c256cbaf47d20eb15b1e8b1a1
Oleg <cccaavvv@gmail.com>
2023-02-13 09:31:27 +0300

    Solve the square equasion

--- HWl1.py
+++ HWl1.py
@@ -1,7 +1,7 @@
 a, b, c = input().split()
 if "\\" in a:
     a = a.split('\\')
-    a = int(a[0])/int(a[1])
+    a = int(a[0]) / int(a[1])
 elif "/" in a:
     a = a.split('/')
     a = int(a[0]) / int(a[1])
@@ -13,7 +13,7 @@ else:
 
 if "\\" in b:
     b = b.split('\\')
-    b = int(b[0])/int(b[1])
+    b = int(b[0]) / int(b[1])
 elif "/" in b:
     b = b.split('/')
     b = int(b[0]) / int(b[1])
@@ -25,7 +25,7 @@ else:
 
 if "\\" in c:
     c = c.split('\\')
-    c = int(c[0])/int(c[1])
+    c = int(c[0]) / int(c[1])
 elif "/" in c:
     c = c.split('/')
     c = int(c[0]) / int(c[1])
@@ -36,13 +36,13 @@ else:
     c = int(c)
 
 print(f'({a})*x^2 + ({b})*x + ({c}) = 0 - введенное квадратное уравнение')
-d = (b**2 - 4*a*c)**(1/2)
-if not (isinstance(d,int) or isinstance(d,float)):
+d = (b ** 2 - 4 * a * c) ** (1 / 2)
+if not (isinstance(d, int) or isinstance(d, float)):
     print('Корней в рациональных числах нет')
-elif d>0:
-    x1 = ((b*(-1) + d)/(2*a))
-    x2 = ((b*(-1) - d)/(2*a))
-    print("Корни данного уравнения",x1,x2)
-elif d==0:
-    x = ((b*(-1) + d)/(2*a))
+elif d > 0:
+    x1 = ((b * (-1) + d) / (2 * a))
+    x2 = ((b * (-1) - d) / (2 * a))
+    print("Корни данного уравнения", x1, x2)
+elif d == 0:
+    x = ((b * (-1) + d) / (2 * a))
     print("Корень данного уравнения", x)
